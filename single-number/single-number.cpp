80514ba539386ebbd3c4cd60ad9b12a219123bc3
ramprasanna-2000 <58840040+ramprasanna-2000@users.noreply.github.com>
2021-06-26 18:00:04 +0530

    Time: 16 ms (84.45%), Space: 17 MB (50.07%) - LeetHub

---
+++ single-number/single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int xor_value = 0;
+        for(int i = 0; i < n;i++)
+        {
+            xor_value = xor_value ^ nums[i];
+        }
+        return xor_value;
+    }
+};
